6db7f77d1adf8e86a17690802942579ff10f3981
Bogdan <bodya8715@gmail.com>
2021-08-02 23:25:41 +0300

    Fix class Worker

--- script/index.js
+++ script/index.js
@@ -1,29 +1,32 @@
+'use strict';
+
+const PREMIUM_CASH = 1.25;
+
 class Worker {
     constructor (firstName, lastName, wokDays, dailySalary, cashBonus) {
+        if( (typeof firstName !== "string") || (typeof lastName !== "string") ) {
+            throw new TypeError('Свойства firstName и lastName необходимо ввести как строку');
+        }
+
         this.firstName = firstName;
         this.lastName = lastName;
 
-        if( (typeof firstName !== "string") || (typeof lastName !== "string") ) {
-            throw new TypeError('Invalid data type for Name');
+        if( isNaN(wokDays - dailySalary) || (typeof wokDays !== "number") || (typeof dailySalary !== "number")) {
+            throw new TypeError('Свойства wokDays и dailySalary необходимо ввести как число');
+        }
+
+        if (wokDays < 0 || dailySalary < 0) {
+            throw new RangeError('Неправильный диапазон данных для свойства wokDays или dailySalary');
         }
 
         this.wokDays = wokDays;
         this.dailySalary = dailySalary;
 
-        if( isNaN(wokDays - dailySalary) || (typeof wokDays !== "number") || (typeof dailySalary !== "number")) {
-            throw new TypeError('Invalid data type for wokDays or dailySalary');
-        }
-
-        if (wokDays < 0 || dailySalary < 0) {
-            throw new RangeError('Invalid data range for wokDays or dailySalary');
+        if ( typeof cashBonus !== "boolean" ) {
+            throw new TypeError('Свойство cashBonus необходимо ввести как логическое значение');
         }
 
         this.cashBonus = cashBonus;
-
-        if ( typeof cashBonus !== "boolean" ) {
-            throw new TypeError('Invalid data type for cashBonus');
-        }
-
     }
 
     getFullName() {
@@ -31,13 +34,11 @@ class Worker {
     }
 
     getSalary() {
-        return this.wokDays*this.dailySalary;
-    }
-
-    setCashBonus() {
-        if (cashBonus) {
-            this.dailySalary*=1.25;
+        if(this.cashBonus) {
+            return this.wokDays * this.dailySalary * PREMIUM_CASH;
         }
+
+        return this.wokDays*this.dailySalary;
     }
  
 }
